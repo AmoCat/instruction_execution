cca3ed1d7db12d41a3c3bf416e6045e0348185e4
AmoCat <jqzhu@ir.hit.edu.cn>
2016-10-27 19:37:47 +0800

    公交信息显示排版正常

--- language_understanding/baidu_API.py
+++ language_understanding/baidu_API.py
@@ -15,6 +15,55 @@ URL_SUFFIX_V1 = "/direction/v1"
 URL_SUFFIX_V2 = "/direction/v2"
 ERROR_CODE = {"1":"Parameter Error","2":"Permission denied"}
 
+class Scheme(object):
+    def __init__(self, s):
+        self.distance = s['distance']
+        self.duration = s['duration']
+        self.line_price = s['line_price']
+        self.bus_price = s['price']
+        steps = s['steps']
+        if len(steps) >= 3:
+            self.walk_from = unicode(Step(steps[0][0]))
+            self.walk_to = unicode(Step(steps[-1][0]))
+            self.bus_ids = Step(steps[1][0]).get_bus_ids()
+            self.steps = []
+            for list in steps[1:-1]:
+                for i in range(len(list)):
+                    self.steps.append(Step(list[i]))
+        else:
+            self.walk_from = None
+            self.walk_to = None
+            self.steps = None
+            self.bus_ids = None
+
+    def __unicode__(self):
+        res = u"距离:%s 公里\t耗时:%s\t票价:¥%s\n"\
+                % (self.cal_dis(self.distance), self.cal_time(self.duration), self.cal_price(self.bus_price))
+        res += u"以下路线需先" + self.walk_from + "\n" if self.walk_from != None else ""
+        if self.steps != None:
+            for i in range(len(self.steps)):
+                res += self.bus_ids[i] + u"\t"
+                res += unicode(self.steps[i])
+        res += self.walk_to + u"至终点" if self.walk_to != None else ""
+        return res
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def cal_dis(self, d):
+        return d/1000.0
+
+    def cal_price(self, p):
+        return p/200
+    
+    def cal_time(self, t):
+        h = t/3600
+        m = (t - h*3600)/60
+        t = t % 60
+        res = str(t) + u"秒"
+        res = str(m) + u"分" + res if m > 0 else res
+        res = str(h) + u"时" + res if h > 0 else res
+        return res
 
 class Step(object):
     def __init__(self, step):
@@ -26,20 +75,25 @@ class Step(object):
         self.stepDestinationLocation = step['stepDestinationLocation']
         self.path = step['path'] if step.has_key('path') else None
         self.vehicle = Vehicle(step['vehicle']) if step['vehicle'] != None else None
-        color = re.compile("<[/]*[a-z]*[\s]*[a-z]*[=\"#]*[0-9]*[\"]*>")
+        color = re.compile("<[/]*[a-z]*[\s]*[a-z]*[=\"#]*[a-z0-9]*[\"]*>")
+        bus_id_compile = re.compile(u"[0-9a-z]*路")
+        direction_compile = re.compile(u"(.*)")
         if step.has_key('stepInstruction'):
-            self.stepInstruction = re.sub(color,'',step['stepInstruction'])
+            self.stepInstruction = re.sub(color,'', step['stepInstruction'])
+            self.bus_ids = re.findall(bus_id_compile, step['stepInstruction'])
+            self.direction = re.search(direction_compile, step['stepInstruction'])
         else:
             self.stepInstruction = None
+            self.bus_ids = None
+            self.direction = None
 
     def __unicode__(self):
         if self.type != 5:
-            res = u"距离:%s公里\t耗时：%s\t%s\n" % (self.cal_dis(self.distance), self.duration, self.stepInstruction)
+            res = u"耗时：%s\t" % (self.duration)
             if self.vehicle != None:
                 res += unicode(self.vehicle)
         elif self.type == 5:
-            res = self.stepInstruction + "\t"
-        res += "\n"
+            res = self.stepInstruction
         return res
 
     def __str__(self):
@@ -50,13 +104,16 @@ class Step(object):
         m = (time - h*3600)/60
         t = time % 60
 
-        res = str(m) + u"秒"
-        res = str(t) + u"分" + res if m > 0 else res
+        res = str(t) + u"秒"
+        res = str(m) + u"分" + res if m > 0 else res
         res = str(h) + u"时" + res if h > 0 else res
         return res
 
     def cal_dis(self, dis):
         return dis/1000.0
+
+    def get_bus_ids(self):
+        return self.bus_ids
 
 class Vehicle(object):
     def __init__(self, v):
@@ -75,9 +132,10 @@ class Vehicle(object):
         self.type = self.type_hash[v['type']]
         self.zone_price = v['zone_price']
 
-    def __unicode__(self):
-        res = u"首班车:%s\t末班车:%s\n" % (self.start_time, self.end_time)
-        res += u"%s站 上车 - %s站 下车 共 %s 站" % (self.start_name, self.end_name, self.stop_num)
+       
+    def __unicode__(self): 
+        res = u"从 %s 上车 - %s 下车 共 %s 站\t" % (self.start_name, self.end_name, self.stop_num)
+        res += u"首班车:%s\t末班车:%s\n" % (self.start_time, self.end_time)
         return res
 
     def __str__(self):
@@ -125,20 +183,23 @@ class baiduAPI(object):
         f = open("scheme_format.txt",'w')
         for v in routes:
             schemes = v["scheme"]
-            for scheme in schemes:
+            for s in schemes:
                 "scheme is a dictionary"
-                print >> f,json.dumps(scheme, sort_keys = True, indent = 4)
-                steps = scheme['steps']
-                for v in steps:
-                    for step in v:
-                        s = Step(step)
-                        print s 
+                scheme = Scheme(s)
+                print scheme
+                return
+                #print >> f,json.dumps(scheme, sort_keys = True, indent = 4)
+                #steps = scheme['steps']
+                #for v in steps:
+                #    for step in v:
+                #        s = Step(step)
+                #        print s 
 
     def get_info(self, query):
-        url = self.make_query_url(**query)
-        data = self.request(url)
-        pkl.dump(data, open('bus_info','w'))
-        #data = pkl.load(open('bus_info', 'r'))
+        #url = self.make_query_url(**query)
+        #data = self.request(url)
+        #pkl.dump(data, open('bus_info','w'))
+        data = pkl.load(open('bus_info', 'r'))
         if query['mode'] == "transit":
             return self.get_bus_info(data)
 
